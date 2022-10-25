22ce5c78842538236ed5fa96eb07ad2065cec6ab
PawelRa <pawelrat@poczta.onet.pl>
2022-10-25 17:11:11 +0200

    debug

---
+++ src/debug/House.java
@@ -0,0 +1,42 @@
+package debug;
+
+public class House {
+    private double area;
+    private String address;
+
+    public House(double area, String address) {
+        constructArea(area);
+        assignAddress(address);
+    }
+
+    private void assignAddress(String address) {
+        this.address = address;
+    }
+
+    private void constructArea(double area) {
+        System.out.println("Constructing area...");
+        buildWalls(8);
+        if (area > 0 && area < 10) {
+            this.area = area * 3;
+
+        } else {
+            this.area = area;
+        }
+        System.out.println("Area constructed.");
+    }
+
+    private void buildWalls(int bricksAmount) {
+        for (int i = 0; i < bricksAmount; i++) {
+            System.out.println("Building brick no " + (i + 1));
+        }
+        System.out.println("Walls built");
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
---
+++ src/debug/Main.java
@@ -0,0 +1,10 @@
+package debug;
+
+public class Main {
+    public static void main(String[] args) {
+        HouseBuilder houseBuilder = new HouseBuilder();
+        House house = houseBuilder.buildHouse(5.0,"Zajavkowa 7/5, Tarnowo Podgorne");
+        System.out.println(house.getArea());
+        System.out.println(house.getAddress());
+    }
+}
